507cb2f5ed6e943f6a564117df8d1ca153fbc98a
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-03 16:06:01 +0100

    Added state data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Feed the birds',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Buy chocolate',
+        completed: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
