97c32f6dc74b6c1de398e36ffbb019b3be801dab
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-12-03 16:22:43 +0100

    Created TodoItem component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
   render() {
-    return (
-      <div>
-       <h1>Todos</h1>
-      </div>
-    );
+    return this.props.todos.map((todo) => (
+      <h3>{todo.title}</h3>
+    ));
   }
 }
 export default Todos;
